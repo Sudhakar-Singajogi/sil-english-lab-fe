76c77a64ed1e199708b11b2f1f7fbef364492441
sudhakarsingajogi <ssr.sudhakar@gmail.com>
2025-04-06 19:43:37 +0530

    added git url to packagejson

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/sil-english-lab-fe/',  // <-- this is the fix
   plugins: [react()],
 })
