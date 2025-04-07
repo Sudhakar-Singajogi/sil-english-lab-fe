25949fa12012a82ed622a03e0e1fa183680ae7e7
sudhakarsingajogi <ssr.sudhakar@gmail.com>
2025-04-07 10:22:02 +0530

    CourseOverview and courseListing UI Design done

--- src/components/ChapterList.jsx
+++ src/components/ChapterList.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import './ChapterList.css';
+import React from "react";
+import "./ChapterList.css";
 
 const chapters = [
   {
     id: 1,
-    title: 'Greetings and Introductions',
-    icon: '/assets/icons/greeting.svg',
-    description: 'Learn how to greet and introduce yourself in English.',
+    title: "Greetings and Introductions",
+    icon: `${import.meta.env.BASE_URL}assets/icons/greeting.svg`,
+    description: "Learn how to greet and introduce yourself in English.",
   },
   {
     id: 2,
-    title: 'The English Alphabet',
-    icon: '/assets/icons/alphabet.svg',
-    description: 'Master the letters of the English alphabet and pronunciation.',
+    title: "The English Alphabet",
+    icon: `${import.meta.env.BASE_URL}assets/icons/greeting.svg`,
+    description:
+      "Master the letters of the English alphabet and pronunciation.",
   },
   {
     id: 3,
-    title: 'Numbers and Counting',
-    icon: '/assets/icons/numbers.svg',
-    description: 'Understand numbers and basic counting in everyday situations.',
+    title: "Numbers and Counting",
+    icon: `${import.meta.env.BASE_URL}assets/icons/greeting.svg`,
+    description:
+      "Understand numbers and basic counting in everyday situations.",
   },
   {
     id: 4,
-    title: 'Days of the Week',
-    icon: '/assets/icons/calendar.svg',
-    description: 'Familiarize yourself with weekdays and how to use them.',
-  }
+    title: "Days of the Week",
+    icon: `${import.meta.env.BASE_URL}assets/icons/greeting.svg`,
+    description: "Familiarize yourself with weekdays and how to use them.",
+  },
 ];
 
 const ChapterList = () => {
@@ -37,10 +39,17 @@ const ChapterList = () => {
           <li key={chapter.id} className="chapter-card">
             <div className="chapter-info">
               <div className="chapter-title">
-                <img src={chapter.icon} alt="icon" className="chapter-icon" />
                 <div>
                   <span className="chapter-number">Chapter {chapter.id}</span>
-                  <h3 className="chapter-name">{chapter.title}</h3>
+
+                  <h3 className="chapter-name">
+                    <img
+                      src={chapter.icon}
+                      alt="icon"
+                      className="chapter-icon"
+                    />
+                    <span className='chapter-title-inner'>{chapter.title}</span>
+                  </h3>
                   <p className="chapter-description">{chapter.description}</p>
                 </div>
               </div>
